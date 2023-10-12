118dddd2a0f1eb987ba1d8dd1b3f7b25372fdb98
bitposh <118928750+bitposh@users.noreply.github.com>
2023-10-12 13:29:55 -0500

    Added Npact

--- src/Program.cs
+++ src/Program.cs
@@ -47,7 +47,8 @@ record Album(int Id, string Title, string Artist, double Price, string Image_url
             new Album(3, "Scale It Up", "KEDA Club", 13.99, "https://aka.ms/albums-kedalogo"),
             new Album(4, "Lost in Translation", "MegaDNS", 12.99,"https://aka.ms/albums-envoylogo"),
             new Album(5, "Lock Down Your Love", "V is for VNET", 12.99, "https://aka.ms/albums-vnetlogo"),
-            new Album(6, "Sweet Container O' Mine", "Guns N Probeses", 14.99, "https://aka.ms/albums-containerappslogo")
+            new Album(6, "Sweet Container O' Mine", "Guns N Probeses", 14.99, "https://aka.ms/albums-containerappslogo"),
+            new Album(7, "Npact's Greatest Hits", "Amazing", 500.00, "https://d3v0px0pttie1i.cloudfront.net/uploads/organization/logo/9138563/35b0412c.jpg")
          };
 
         return albums; 
