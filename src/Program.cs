f3c1b3042a3c9b63e692fc94d0a6a74fed220733
bitposh <118928750+bitposh@users.noreply.github.com>
2023-10-17 15:37:33 -0500

    removed man u

--- src/Program.cs
+++ src/Program.cs
@@ -48,8 +48,7 @@ record Album(int Id, string Title, string Artist, double Price, string Image_url
             new Album(4, "Lost in Translation", "MegaDNS", 12.99,"https://aka.ms/albums-envoylogo"),
             new Album(5, "Lock Down Your Love", "V is for VNET", 12.99, "https://aka.ms/albums-vnetlogo"),
             new Album(6, "Sweet Container O' Mine", "Guns N Probeses", 14.99, "https://aka.ms/albums-containerappslogo"),
-            new Album(7, "Npact's Greatest Hits", "Amazing", 500.00, "https://d3v0px0pttie1i.cloudfront.net/uploads/organization/logo/9138563/35b0412c.jpg"),
-            new Album(8, "Man U","Mid Table", 7000000000, "https://static.wikia.nocookie.net/logopedia/images/8/86/Manchester_United_FC_logo_%282D%29.svg")
+            new Album(7, "Npact's Greatest Hits", "Amazing", 500.00, "https://d3v0px0pttie1i.cloudfront.net/uploads/organization/logo/9138563/35b0412c.jpg")
          };
 
         return albums; 
